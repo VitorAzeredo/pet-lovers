aa3ff131d082ba3f76d8e362b5f4551140df9633
Vitor Azeredo <39863605+VitorAzeredo@users.noreply.github.com>
2022-05-08 19:36:28 -0300

    Update na pagina de parceiros e contatos

--- pages/contatos/index.jsx
+++ pages/contatos/index.jsx
@@ -7,15 +7,79 @@ export default function About() {
 			<Head>
 				<title>Contatos</title>
 			</Head>
-			<main>
-				<div className="container-fluid pt-4 ps-5 pe-5 pb-4">
-					<Navbar />
-					<div className="row">
-						<div className="col-12 text-center">
-							<h1>Adorariamos ouvir sua opnião!</h1>
-						</div>
-					</div>
-				</div>
+			<main class="flex-shrink-0">
+				<section class="py-4 ps-5 pe-5">
+				<Navbar />
+                <div class="container px-5">
+                    <div class="bg-light rounded-3 py-5 px-4 px-md-5 mb-5">
+                        <div class="text-center mb-5">
+                            <div class="rounded-3 mb-3"><i class="bi bi-envelope-heart-fill-5x"></i></div>
+                            <h1 class="fw-bolder">Vamos Conversar!</h1>
+                            <p class="lead fw-normal text-muted mb-0">Sua opnião e muito importante pra nós, sugestões ou criticas.</p>
+                        </div>
+                        <div class="row gx-5 justify-content-center">
+                            <div class="col-lg-8 col-xl-6">
+                                <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                                    <div class="form-floating mb-3">
+                                        <input class="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
+                                        <label for="name">Seu Nome</label>
+                                        <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                                    </div>
+                                    <div class="form-floating mb-3">
+                                        <input class="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
+                                        <label for="email">Um Email valido</label>
+                                        <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
+                                        <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
+                                    </div>
+                                    <div class="form-floating mb-3">
+                                        <input class="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
+                                        <label for="phone">Um numero de contato</label>
+                                        <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
+                                    </div>
+                                    <div class="form-floating mb-3">
+                                        <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..." data-sb-validations="required"></textarea>
+                                        <label for="message">Deixe sua Mensagem</label>
+                                        <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
+                                    </div>
+                                    <div class="d-none" id="submitSuccessMessage">
+                                        <div class="text-center mb-3">
+                                            <div class="fw-bolder">Form submission successful!</div>
+                                            To activate this form, sign up at
+                                            <br />
+                                            <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                        </div>
+                                    </div>
+                                    <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
+                                    <div class="d-grid"><button class="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button></div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+					{/*}
+                    <div class="row gx-5 row-cols-2 row-cols-lg-4 py-5">
+                        <div class="col">
+                            <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-chat-dots"></i></div>
+                            <div class="h5 mb-2">Chat with us</div>
+                            <p class="text-muted mb-0">Chat live with one of our support specialists.</p>
+                        </div>
+                        <div class="col">
+                            <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-people"></i></div>
+                            <div class="h5">Ask the community</div>
+                            <p class="text-muted mb-0">Explore our community forums and communicate with other users.</p>
+                        </div>
+                        <div class="col">
+                            <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-question-circle"></i></div>
+                            <div class="h5">Support center</div>
+                            <p class="text-muted mb-0">Browse FAQ's and support articles to find solutions.</p>
+                        </div>
+                        <div class="col">
+                            <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-telephone"></i></div>
+                            <div class="h5">Call us</div>
+                            <p class="text-muted mb-0">Call us during normal business hours at (555) 892-9403.</p>
+                        </div>
+	</div>*/}
+                </div>
+            	</section>
 			</main>
 		</div>
 	);
--- pages/parceiros/index.jsx
+++ pages/parceiros/index.jsx
@@ -8,35 +8,66 @@ export default function About() {
 				<title>Serviços</title>
 			</Head>
 			<main class="flex-shrink-0">
-				<section class="py-5">
+				<section class="py-4 mb-3 ps-5 pe-5">
+					<Navbar />
             	    <div class="container px-5 my-5">
             	        <div class="text-center mb-5">
-            	            <h1 class="fw-bolder">Our Work</h1>
-            	            <p class="lead fw-normal text-muted mb-0">Company portfolio</p>
+            	            <h1 class="fw-bolder">Nossos Parceiros</h1>
+            	            <p class="lead fw-normal text-muted mb-0">Empresas e Marcas que acreditam em nosso trabalho</p>
             	        </div>
             	        <div class="row gx-5">
-            	            <div class="col-lg-6">
+            	            <div class="col-lg-4">
             	                <div class="position-relative mb-5">
-            	                    <img class="img-fluid rounded-3 mb-3" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." />
-            	                    <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="#!">Project name</a>
+            	                    <img class="img-fluid rounded-3 mb-3" src="/assets/parceiros/parceiro_amazon.jpg" alt="..." />
+            	                    <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="https://www.amazon.com.br/">Amazon</a>
             	                </div>
             	            </div>
-            	            <div class="col-lg-6">
+            	            <div class="col-lg-4">
             	                <div class="position-relative mb-5">
-            	                    <img class="img-fluid rounded-3 mb-3" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." />
-            	                    <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="#!">Project name</a>
+            	                    <img class="img-fluid rounded-3 mb-3" src="/assets/parceiros/parceiro_google.jpg" alt="..." />
+            	                    <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="https://www.google.com/intl/pt-BR_br/business/">Google</a>
             	                </div>
             	            </div>
-            	            <div class="col-lg-6">
+            	            <div class="col-lg-4">
             	                <div class="position-relative mb-5 mb-lg-0">
-            	                    <img class="img-fluid rounded-3 mb-3" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." />
-            	                    <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="#!">Project name</a>
+            	                    <img class="img-fluid rounded-3 mb-3" src="/assets/parceiros/parceiro_facebook.jpg" alt="..." />
+            	                    <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="https://about.facebook.com/br/meta/">Facebook</a>
             	                </div>
             	            </div>
-            	            <div class="col-lg-6">
+            	            <div class="col-lg-4">
             	                <div class="position-relative">
-            	                    <img class="img-fluid rounded-3 mb-3" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." />
-            	                    <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="#!">Project name</a>
+            	                    <img class="img-fluid rounded-3 mb-3" src="/assets/parceiros/parceiro_ambev.jpg" alt="..." />
+            	                    <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="https://www.ambev.com.br/">Ambev</a>
+            	                </div>
+            	            </div>
+							<div class="col-lg-4">
+            	                <div class="position-relative mb-5">
+            	                    <img class="img-fluid rounded-3 mb-3" src="/assets/parceiros/parceiro_cocacola.jpg" alt="..." />
+            	                    <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="https://www.coca-cola.com.br/">Coca-Cola</a>
+            	                </div>
+            	            </div>
+							<div class="col-lg-4">
+            	                <div class="position-relative mb-5">
+            	                    <img class="img-fluid rounded-3 mb-3" src="/assets/parceiros/parceiro_xp.jpg" alt="..." />
+            	                    <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="https://www.xpi.com.br/">XP Investimentos</a>
+            	                </div>
+            	            </div>
+							<div class="col-lg-4">
+            	                <div class="position-relative mb-5">
+            	                    <img class="img-fluid rounded-3 mb-3" src="/assets/parceiros/parceiro_mercado.jpg" alt="..." />
+            	                    <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="https://www.mercadobitcoin.com.br/">Mercado Bitcoin</a>
+            	                </div>
+            	            </div>
+							<div class="col-lg-4">
+            	                <div class="position-relative mb-5">
+            	                    <img class="img-fluid rounded-3 mb-3" src="/assets/parceiros/parceiro_microsoft.jpg" alt="..." />
+            	                    <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="https://www.microsoft.com/pt-br">Microsoft</a>
+            	                </div>
+            	            </div>
+							<div class="col-lg-4">
+            	                <div class="position-relative mb-5">
+            	                    <img class="img-fluid rounded-3 mb-3" src="/assets/parceiros/parceiro_binance.jpg" alt="..." />
+            	                    <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="https://www.binance.com/pt-BR">Binance</a>
             	                </div>
             	            </div>
             	        </div>
