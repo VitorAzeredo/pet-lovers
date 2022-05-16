ab22e385144b5c6f9082d0686568b055895aa688
Nixon Azeredo <nixonazeredo@gmail.com>
2022-05-16 19:21:12 -0300

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,14 @@ import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { auth } from "../core/config/firebase/client";
+import { Offcanvas } from "react-bootstrap";
 
 export default function Home() {
 	const [isLogged, setIsLogged] = useState(false);
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
 	useEffect(() => {
 		auth.onAuthStateChanged((user) => {
@@ -22,7 +27,7 @@ export default function Home() {
 	};
 
 	return (
-		<div class="container-fluid">
+		<div className="container-fluid p-0">
 			<Head>
 				<title>Pet Lovers</title>
 				<meta
@@ -33,20 +38,82 @@ export default function Home() {
 			</Head>
 
 			<main className="fullHeight">
+				<Offcanvas show={show} onHide={handleClose}>
+					<Offcanvas.Header closeButton>
+						<Offcanvas.Title>Menu</Offcanvas.Title>
+					</Offcanvas.Header>
+					<Offcanvas.Body>
+						<p className="mt-3" onClick={handleClose}>
+							{" "}
+							<Link href="/sobre">
+								<a className="customLink">Sobre</a>
+							</Link>
+						</p>
+						<p className="mt-3">
+							{" "}
+							<Link href="/adocao">
+								<a className="customLink">Adoção</a>
+							</Link>
+						</p>
+						<p className="mt-3">
+							{" "}
+							<Link href="/apadrinhamento">
+								<a className="customLink">Apadrinhamento</a>
+							</Link>
+						</p>
+						<p className="mt-3">
+							{" "}
+							<Link href="/artigos">
+								<a className="customLink">Artigos</a>
+							</Link>
+						</p>
+						<p className="mt-3">
+							{" "}
+							<Link href="/parceiros">
+								<a className="customLink">Parceiros</a>
+							</Link>
+						</p>
+						<p className="mt-3">
+							{" "}
+							<Link href="/contatos">
+								<a className="customLink">Contatos</a>
+							</Link>
+						</p>
+						<p className="mt-3 fullWidth ">
+							{" "}
+							{isLogged ? (
+								<Link href="/">
+									<a
+										onClick={signOut}
+										className="customLink rounded"
+									>
+										Sair
+									</a>
+								</Link>
+							) : (
+								<Link href="/autenticacao/entrar">
+									<a className="customLink rounded">
+										Entrar
+									</a>
+								</Link>
+							)}
+						</p>
+					</Offcanvas.Body>
+				</Offcanvas>
 				<div className="container-fluid m-0">
 					<div className="row">
-						<div className={`fullHeight col-6 pt-4 ps-5 pe-5 pb-4`}>
-							<div className="row">
-								<div className="col-6 p-0">
+						<div className={`fullHeight col-sm-6 pt-4 ps-5 pe-5 pb-4`}>
+							<div className="row pt-2">
+								<div className="col-6 p-0 d-flex algn-items-center">
 									{" "}
 									<Link href="/" passHref>
-										<h3 className="customLink">
+										<h3 className="customLink mb-0">
 											Pet-Lovers
 										</h3>
 									</Link>
 								</div>
 								<div className="col-6 p-0 text-end">
-									<i className="bi bi-search-heart fs-3"></i>
+									<i style={{ cursor: 'pointer' }} onClick={handleShow} className="bi bi-list fs-3 d-block d-sm-none"></i>
 								</div>
 							</div>
 							<div className="row mt-5">
@@ -100,45 +167,48 @@ export default function Home() {
 							</div>
 						</div>
 						<div
-							className={`${styles.bgPrincipalDog} fullHeight col-6 pt-4 ps-5 pe-5 pb-4 justify-content-end d-flex`}
+							className={`${styles.bgPrincipalDog} fullHeight d-none col-sm-6 pt-4 ps-5 pe-5 pb-4 justify-content-end d-sm-flex`}
 						>
-							<p className="ms-3">
+							<p onClick={handleShow} className="mb-0 d-block d-xl-none" style={{ cursor: 'pointer' }}>
+								<i className="bi bi-list fs-1"></i>
+							</p>
+							<p className="ms-3 d-none d-xl-block">
 								{" "}
 								<Link href="/sobre">
 									<a className="customLink">Sobre</a>
 								</Link>
 							</p>
-							<p className="ms-3">
+							<p className="ms-3 d-none d-xl-block">
 								{" "}
 								<Link href="/adocao">
 									<a className="customLink">Adoção</a>
 								</Link>
 							</p>
-							<p className="ms-3">
+							<p className="ms-3 d-none d-xl-block">
 								{" "}
 								<Link href="/apadrinhamento">
 									<a className="customLink">Apadrinhamento</a>
 								</Link>
 							</p>
-							<p className="ms-3">
+							<p className="ms-3 d-none d-xl-block">
 								{" "}
 								<Link href="/artigos">
 									<a className="customLink">Artigos</a>
 								</Link>
 							</p>
-							<p className="ms-3">
+							<p className="ms-3 d-none d-xl-block">
 								{" "}
 								<Link href="/parceiros">
 									<a className="customLink">Parceiros</a>
 								</Link>
 							</p>
-							<p className="ms-3">
+							<p className="ms-3 d-none d-xl-block">
 								{" "}
 								<Link href="/contatos">
 									<a className="customLink">Contatos</a>
 								</Link>
 							</p>
-							<p className="ms-3">
+							<p className="ms-3 d-none d-xl-block">
 								{" "}
 								{isLogged ? (
 									<Link href="/">
--- shared/components/Navbar/index.jsx
+++ shared/components/Navbar/index.jsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
 
 import { useEffect, useState } from "react";
+import { Offcanvas } from "react-bootstrap";
 import { auth } from "../../../core/config/firebase/client";
 
 export default function Navbar() {
 	const [isLogged, setIsLogged] = useState(false);
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
 	useEffect(() => {
 		auth.onAuthStateChanged((user) => {
@@ -21,50 +26,115 @@ export default function Navbar() {
 	};
 	return (
 		<div className="row">
-			<div className="col-6 p-0">
+			<Offcanvas show={show} onHide={handleClose}>
+				<Offcanvas.Header closeButton>
+					<Offcanvas.Title>Menu</Offcanvas.Title>
+				</Offcanvas.Header>
+				<Offcanvas.Body>
+					<p className="mt-3" onClick={handleClose}>
+						{" "}
+						<Link href="/sobre">
+							<a className="customLink">Sobre</a>
+						</Link>
+					</p>
+					<p className="mt-3">
+						{" "}
+						<Link href="/adocao">
+							<a className="customLink">Adoção</a>
+						</Link>
+					</p>
+					<p className="mt-3">
+						{" "}
+						<Link href="/apadrinhamento">
+							<a className="customLink">Apadrinhamento</a>
+						</Link>
+					</p>
+					<p className="mt-3">
+						{" "}
+						<Link href="/artigos">
+							<a className="customLink">Artigos</a>
+						</Link>
+					</p>
+					<p className="mt-3">
+						{" "}
+						<Link href="/parceiros">
+							<a className="customLink">Parceiros</a>
+						</Link>
+					</p>
+					<p className="mt-3">
+						{" "}
+						<Link href="/contatos">
+							<a className="customLink">Contatos</a>
+						</Link>
+					</p>
+					<p className="mt-3 fullWidth ">
+						{" "}
+						{isLogged ? (
+							<Link href="/">
+								<a
+									onClick={signOut}
+									className="customLink rounded"
+								>
+									Sair
+								</a>
+							</Link>
+						) : (
+							<Link href="/autenticacao/entrar">
+								<a className="customLink rounded">
+									Entrar
+								</a>
+							</Link>
+						)}
+					</p>
+				</Offcanvas.Body>
+			</Offcanvas>
+			<div className="col-6 p-0 d-flex align-items-center">
 				{" "}
 				<Link href="/" passHref>
 					<h3 className="customLink">Pet-Lovers</h3>
 				</Link>
 			</div>
 			<div className="col-6 justify-content-end d-flex">
-				<p className="ms-3">
+				<p onClick={handleShow} className="mb-0 d-block d-md-none" style={{ cursor: 'pointer' }}>
+					<i className="bi bi-list fs-1"></i>
+				</p>
+				<p className="ms-3 d-none d-md-block">
 					{" "}
 					<Link href="/sobre">
 						<a className="customLink">Sobre</a>
 					</Link>
 				</p>
-				<p className="ms-3">
+				<p className="ms-3 d-none d-md-block">
 					{" "}
 					<Link href="/adocao">
 						<a className="customLink">Adoção</a>
 					</Link>
 				</p>
-				<p className="ms-3">
+				<p className="ms-3 d-none d-md-block">
 					{" "}
 					<Link href="/apadrinhamento">
 						<a className="customLink">Apadrinhamento</a>
 					</Link>
 				</p>
-				<p className="ms-3">
+				<p className="ms-3 d-none d-md-block">
 					{" "}
 					<Link href="/artigos">
 						<a className="customLink">Artigos</a>
 					</Link>
 				</p>
-				<p className="ms-3">
+				<p className="ms-3 d-none d-md-block">
 					{" "}
 					<Link href="/parceiros">
 						<a className="customLink">Parceiros</a>
 					</Link>
 				</p>
-				<p className="ms-3">
+				<p className="ms-3 d-none d-md-block">
 					{" "}
 					<Link href="/contatos">
 						<a className="customLink">Contatos</a>
 					</Link>
 				</p>
-				<p className="ms-3">
+				<p className="ms-3 d-none d-md-block">
 					{" "}
 					{isLogged ? (
 						<Link href="/">
